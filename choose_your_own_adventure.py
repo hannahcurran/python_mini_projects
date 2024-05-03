7a98cb9bd9f1582958bead2f4865b968842a5c1d
Hannah Curran <hannah.l.curran@gmail.com>
2024-05-02 18:13:36 -0700

    first commit

---
+++ choose_your_own_adventure.py
@@ -0,0 +1,43 @@
+name = input("Type your name: ")
+print("Welcome", name, "to this adventure!")
+
+answer = input(
+    "You are on a dirt road, it has come to an end and you can go left or right. Which way would you like to go?").lower()
+
+if answer == "left":
+    answer = input(
+        "You come to a river, you can walk around it or swim across? Type walk to walk around or swim to swim across.")
+
+    if answer == "swim":
+        print("You swam across and were eaten by an alligator.")
+
+    elif answer == "walk":
+        print("You walked for many miles, ran out of water and you lost the game.")
+
+    else:
+        print("Not a valid option - you lose.")
+
+elif answer == "right":
+    answer = input(
+        "You come to a bridge, it looks wobbly, do you want to cross it or head back?(Type cross / back)")
+
+    if answer == "back":
+        print("You go back and lose.")
+
+    elif answer == "cross":
+        answer = input(
+            "You cross the bridge and meet a stranger. Do you talk to them? yes / no")
+        if answer == "yes":
+            print("You talk to a stranger and they give you gold. You win!")
+
+        elif answer == "no":
+            print("You ignore the stranger and they are offended and you lose.")
+
+        else:
+            print("Not a valid option - you lose.")
+
+    else:
+        print("Not a valid option - you lose.")
+
+else:
+    print("Not a valid option - you lose.")
